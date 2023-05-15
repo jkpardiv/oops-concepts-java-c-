92a0d0bf341030ced386e9c5d40228c01271c8cf
KOUSHIK PARDHIVU JUPALLI <jkpardiv@gmail.com>
2023-05-15 05:31:14 +0000

    Upload New File

---
+++ Arraylist2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Arraylist2{
+    public static void main(String[] args){
+        ArrayList<String> items=new  ArrayList<String>();
+        items.add("rohit");
+        items.add("virat");
+        items.add("surya");
+        //to return number elements in array we use size method
+        System.out.println(items.size());
+    }
+}
