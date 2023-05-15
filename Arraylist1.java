74118f721125b13c22ffe5e04d399857df452d46
KOUSHIK PARDHIVU JUPALLI <jkpardiv@gmail.com>
2023-05-15 05:29:07 +0000

    Upload New File

---
+++ Arraylist1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Arraylist1{
+    public static void main(String[] args){
+        ArrayList<String> items=new  ArrayList<String>();
+        items.add("rohit");
+        items.add("virat");
+        items.add("surya");
+        System.out.println(items);
+    }
+}
