17822ff11b03bbce6e68822618600bf307c7fdff
KOUSHIK PARDHIVU JUPALLI <jkpardiv@gmail.com>
2023-06-02 11:29:44 +0000

    Upload New File

---
+++ BoxandUnboxing.java
@@ -0,0 +1,11 @@
+class BoxandUnboxing{
+    public static void main(String[] args){
+        int a=45;
+        Integer k=a;
+        Integer a2=18;
+         int a3=k;
+        System.out.println(k+" "+a2);
+        System.out.println(a2+" "+a3);
+
+    }
+}
